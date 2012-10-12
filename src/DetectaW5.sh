c643daca22116eba9abae72b821fee9abfdad9ac
gabrieljaimerena <gabrieljaimerena@gmail.com>
2012-10-12 16:03:01 +0000

    Subo la version completa de DetectarW5, ya cumple con todo lo pedido.
    Falta modificar la parte de Mover y Buscar, pero recien cuando esten listos.
    
    Si quieren pruebenlo, asi lo vamos integrando.
    
    Saludos

--- src/DetectaW5.sh
+++ src/DetectaW5.sh
@@ -3,6 +3,7 @@
 #variable interna
 sis_valido=false
 error_ambiente=false
+codigo_rechazo=0
 
 #verifico inicializacion de ambiente
 #verifico que las variables no sean nulas
@@ -41,15 +42,15 @@ while [ -e ./.cont_temp ] #mientras exista el archivo temporal cont_temp se cont
 do
 
 	#guardo archivos de ARRIDIR en un temporal
-	ls -1p $GRUPO$ARRIDIR | grep -v /\$ > archivos_temp
+	ls -1p $GRUPO$ARRIDIR | grep -v /\$ > .archivos_temp
 
 
 	#cuento cantidad de archivos
-	cant_archivos=$(wc -l < archivos_temp)
+	cant_archivos=$(wc -l < .archivos_temp)
 	
 	# obtengo todo los codigos de sistema con su fecha de alta y baja
-	cut -f1,3,4 -d',' $GRUPO/MAEDIR/sistemas > cod_sis_temp
-	cant_sistemas=$(wc -l < cod_sis_temp)
+	cut -f1,3,4 -d',' $GRUPO/MAEDIR/sistemas > .cod_sis_temp
+	cant_sistemas=$(wc -l < .cod_sis_temp)
 	
 	#recorro cada archivo de ARRIDIR y los valido contra MAEDIR/sistemas
 	for i in $(seq 1 $cant_archivos)
@@ -57,7 +58,7 @@ do
 	   
 	    sis_valido=false
 	    #obtengo nombre del primer archivo
-	    archivo=$(head -n $i archivos_temp | tail -n 1)
+	    archivo=$(head -n $i .archivos_temp | tail -n 1)
 	
 	   
 	    #valido formato codigoSistema_aaaa-mm-dd
@@ -74,9 +75,6 @@ do
 			    mes=$(echo $fecha |cut -d \- -f 2)
 			    dia=$(echo $fecha |cut -d \- -f 3)
 
-			    
-			   
-
 			    if [ $anio -ge 2000 -a $anio -le $(date +%Y) ]; then 
 			    # invalido si el año no va de 2000 al actual
 				if [ $mes -ge 1 -a $mes -le 12 ]; then 
@@ -88,46 +86,67 @@ do
 				    fi
 				fi
 			    fi
+			    
 	
 			    if [ $fecha_valida != false ]; then
 			
 				    #trasformo fecha a formato para comparar
 				    fecha_arch=$(echo $fecha | sed "s-\---g")
+				    
+				    sis_hallado=false
 
 				    #recorro archivo de sistemas para validar sis_id y fechas
 				    for j in $(seq 1 $cant_sistemas)
 				    do
-					sistema=$(head -n $j cod_sis_temp | tail -n 1)
+					sistema=$(head -n $j .cod_sis_temp | tail -n 1)
 					#separo los campos	
 					sis_id=$(echo $sistema | cut -d \, -f 1)	
 					fecha_alta=$(echo $sistema | cut -d \, -f 2)
 					fecha_baja=$(echo $sistema | cut -d \, -f 3 )
-	
+					
 					fecha_alta_sist=$(echo $fecha_alta | sed "s-\---g")
 					fecha_baja_sist=$(echo $fecha_baja | sed "s-\---g")
-	
+
 					#obtengo fecha actual en el formato que necesito
 					fecha_actual=$(echo $(date +%Y)$(date +%m)$(date +%d))	
 	
 					# valido que el id del sistema exista
 					if [ $sis_id -eq $sis_id1 ]; then
+					    sis_hallado=true
 					    if [ $fecha_arch -le $fecha_actual ]; then
 						if [ $fecha_arch -ge $fecha_alta_sist ]; then
 						    if [ ! -z $fecha_baja ]; then
 							if [ $fecha_arch -le $fecha_baja_sist ]; then
+							    			    
 							    sis_valido=true
+							else
+							    codigo_rechazo=7
 							fi
 						    else
 							sis_valido=true
 						    fi
+						else
+						    codigo_rechazo=6						
 						fi
+					    else
+					    	codigo_rechazo=5
 					    fi
 					fi
 
 				    done # for - de sistemas
+				    
+				    if [ $sis_hallado == false ]; then
+					codigo_rechazo=3
+				    fi
+			    else
+				codigo_rechazo=4
 			    fi
+		    else
+		    	codigo_rechazo=2
 		    fi
 	    
+	    else
+		codigo_rechazo=1
 	    fi
 
 
@@ -142,11 +161,32 @@ do
 		mv $GRUPO$ARRIDIR/$archivo $GRUPO$ACEPDIR/$archivo
 		
 	    else
+		
+		if [ $codigo_rechazo -eq 1 ]; then
+		    mensaje="Error formato del archivo: $archivo. Se movio al directorio de rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 2 ]; then
+		    mensaje="Tipo archivo invalido: $archivo. Se movio al directorio de rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 3 ]; then
+		    mensaje="No existe sistema asociado al cod_id del archivo $archivo, se movio a rechazados"
+		   sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 4 ]; then
+		    mensaje="Fecha invalida en el $archivo, se movio al directorio de rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 5 ]; then
+		    mensaje="Error, fecha posterior a la actual. Se movio el archivo $archivo a rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 6 ]; then
+		    mensaje="Error, fecha anterior a la de Alta. Se movio el archivo $archivo a rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		elif [ $codigo_rechazo -eq 7 ]; then
+		    mensaje="Error, fecha posterior a la de Baja. Se movio el archivo $archivo a rechazados"
+		    sh LoguearW5.sh "DetectarW5" "E" "$mensaje"
+		fi		
 		#habilitar lo que sigue cuando se integre todo			
 		#sh MoverW5
-		mensaje="Se movio el archivo $archivo al directorio de rechazados"
-		sh LoguearW5.sh "DetectarW5" "I" "$mensaje"	
-			
+					
 		#comentar lo que sigue cuando se integre
 		mv $GRUPO$ARRIDIR/$archivo $GRUPO$RECHDIR/$archivo
 	    fi
@@ -158,9 +198,23 @@ do
 	
 	done #for - de archivos
     
-    #DESCOMENTAR CUANDO FUNCIONE BUSCARW5.SH	
-    #chequeo que buscarw5 se este ejecutando solo una vez
-    #sh BuscarW5.sh     
+
+    #chequeo existencia de archivos en directorio ACEPDIR
+    
+    ls -1p $GRUPO$ACEPDIR | grep -v /\$ > .archivos_acep_temp
+    cant_acep=$(wc -l < .archivos_acep_temp)
+    
+    #si existen archivos en ACEPDIR ejecuto comando BuscarW5.sh
+    if [ $cant_acep -ne 0 ]; then
+	#DESCOMENTAR CUANDO FUNCIONE BUSCARW5.SH	
+	#chequeo que buscarw5 se este ejecutando solo una vez
+	echo "invoque a Buscar.sh"	
+	#sh BuscarW5.sh     
+    fi
+
     sleep 10s
+    rm .archivos_temp
+    rm .cod_sis_temp
+    rm .archivos_acep_temp
 
 done
