de8b48a45160e8b79e018c2a84b435fc6b99d72a
estebaneze <estebaneze@gmail.com>
2012-10-15 03:54:11 +0000

    Log terminado con validaciones de tamano y longitud de mensaje.

--- src/LoguearW5.sh
+++ src/LoguearW5.sh
@@ -2,14 +2,23 @@
 
 #funcion que imprime el correcto uso de la herramienta
 errorParametro() {
-        echo "Error uso LoguearW5"
-        echo "Forma Correcta:"
-        echo "LoguearW5 <comando> <tipoMensaje [I, A, E, SE]> <mensaje>"
+        echo 'Error uso LoguearW5'
+        echo 'Forma Correcta:'
+        echo 'LoguearW5 <comando> <tipoMensaje [I, A, E, SE]> <mensaje>'
+}
+
+errorDirectorioGrupo() {
+        echo 'Por favor, defina variable GRUPO antes de utilizar LoguearW5'
 }
 
 #Estan los parametros requeridos?
-if [ $# -ne 3 ]
-then
+if [ -z "$GRUPO" ]; then
+        errorDirectorioGrupo
+        exit 1
+fi
+
+#Estan los parametros requeridos?
+if [ $# -ne 3 ]; then
         errorParametro
         exit 1
 fi
@@ -18,20 +27,34 @@ comando=$1
 tipoMensaje=$2
 mensaje=$3
 
+#tamano maximo mensaje 120 caracteres
+mensaje=`echo $mensaje | cut -c -120`
+
 #TipoMensaje OK?
-if [ $tipoMensaje != "I" ] && [ $tipoMensaje != "A" ] && [ $tipoMensaje != "E" ] && [ $tipoMensaje != "SE" ]
-then
+if [ $tipoMensaje != "I" ] && [ $tipoMensaje != "A" ] && [ $tipoMensaje != "E" ] && [ $tipoMensaje != "SE" ]; then
         errorParametro
         exit 1
 fi
 
-#extension y directorio estan definidos? Sino => valores default
-if [ -z "$LOGDIR" ]; then
-	LOGDIR="logdir"
+#si comando que llama Instalar => directorio conf
+if [ $comando = "InstalaW5" ]; then
+	LOGDIR="conf"
+else
+	# estan definido? Sino => valor default
+	if [ -z "$LOGDIR" ]; then
+		LOGDIR="logdir"
+	fi
 fi
 
+
+#extension definido? Sino => valor default
 if [ -z "$LOGEXT" ]; then
         LOGEXT="log"
+fi
+
+#tamano definido? Sino => valor default
+if [ -z "$LOGSIZE" ]; then
+        LOGSIZE=100
 fi
 
 #path de la carpeta de logueo
@@ -53,5 +76,23 @@ why=$mensaje
 
 #escribe en el archivo
 echo "$when-$who-$where-$what-$why" >> "$archivoLog"
+
+#valido tamano Log
+tamanoLog=`echo "$(stat -c%s $archivoLog)/1024" | bc`
+
+if [ $tamanoLog -ge "$LOGSIZE" ]; then
+	when=$(date)
+	where="LoguearW5"
+	what="I"
+	why="Log Excedido"
+	totalLineasLog=$(wc -l < "$archivoLog")
+     	mitadLineasLog=`echo "$totalLineasLog/2" | bc`
+
+	#elimino mitad del archivo de log y copio en temporal
+    	sed "1,${mitadLineasLog}d" $archivoLog>${archivoLog}.tmp
+    	mv ${archivoLog}.tmp $archivoLog
+	echo "$when-$who-$where-$what-$why" >> "$archivoLog"
+
+fi
 
 exit 0
