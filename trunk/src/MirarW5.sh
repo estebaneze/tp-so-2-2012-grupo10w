b2871cbc52d4eb39191bf41568747a8d87f5466b
damianfiuba@gmail.com <damianfiuba@gmail.com@3507be77-532f-1d92-cd08-bbd8338995c8>
2012-10-16 11:44:35 +0000

    Primer MirarW5

---
+++ trunk/src/MirarW5.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Primer intento de la funcion loguear
+
+function mirarW5 {
+
+NLINES=
+FIND=
+ONLY=
+
+ARCH=$BASH_ARGV 	# Comprobar solo Bash >= 3.0
+while getopts “g:n:o:0123456789” OPTION
+do
+	case $OPTION in
+		g)
+		FIND=$OPTARG
+		;;
+		n)
+		if [[ $OPTARG =~ ^[0-9]+$ ]] && [[ ! $LINES ]];
+		then
+			NLINES=$OPTARG
+		else
+			echo "Numero de lineas no valido"
+			return 1
+		fi
+		;;
+		o)
+		ONLY=$OPTARG
+		;;
+		[0-9])
+		NLINES=$LINES$OPTION
+		;;
+		?)
+		return 1
+		;;
+	esac
+done
+
+if [[ $NLINES ]] ;
+then
+
+IFS=$'\n'
+for LINE in $(tail -n$NLINES $ARCH | grep ""$FIND); do
+	FECHA=$(echo $LINE | cut -d"-" -f1 | cut -d" " -f1)
+	HORA=$(echo $LINE | cut -d"-" -f1 | cut -d" " -f2)
+	USUARIO=$(echo $LINE | cut -d"-" -f2)
+	COMANDO=$(echo $LINE | cut -d"-" -f3)
+	TIPO=$(echo $LINE | cut -d"-" -f4)
+	MOTIVO=$(echo $LINE | cut -d"-" -f5)
+	
+	if [[ ! $ONLY ]] || [[ $ONLY = $TIPO ]] ; then
+	echo "FECHA: $FECHA	HORA: $HORA"
+	echo "	USUARIO: $USUARIO"
+	echo "	COMANDO: $COMANDO"
+	echo "	TIPO DE MENSAJE: $TIPO"
+	echo "	MOTIVO: $MOTIVO"
+	echo
+	fi
+done
+
+else
+
+IFS=$'\n'
+for LINE in $(cat $ARCH | grep ""$FIND); do
+	FECHA=$(echo $LINE | cut -d"-" -f1 | cut -d" " -f1)
+	HORA=$(echo $LINE | cut -d"-" -f1 | cut -d" " -f2)
+	USUARIO=$(echo $LINE | cut -d"-" -f2)
+	COMANDO=$(echo $LINE | cut -d"-" -f3)
+	TIPO=$(echo $LINE | cut -d"-" -f4)
+	MOTIVO=$(echo $LINE | cut -d"-" -f5)
+
+	if [[ ! $ONLY ]] || [[ $ONLY = $TIPO ]] ; then
+	echo "FECHA: $FECHA	HORA: $HORA"
+	echo "	USUARIO: $USUARIO"
+	echo "	COMANDO: $COMANDO"
+	echo "	TIPO DE MENSAJE: $TIPO"
+	echo "	MOTIVO: $MOTIVO"
+	echo
+	fi
+done
+
+fi
+return 0
+}
+
+
+mirarW5 $1 $2 $3 $4
