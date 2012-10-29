5e44a553c4cb080d9692a2a853a8180c17069196
leandro.v.059 <leandro.v.059@gmail.com>
2012-10-29 23:54:32 +0000

    Subo los arreglo de Damián al Mover

--- src/MoverW5.sh
+++ src/MoverW5.sh
@@ -7,31 +7,31 @@ function moverW5 {
 	if [ $# -lt 2 ]
 	then
 		
-		${BINDIR}/LoguearW5.sh $3 "E" "Error al mover. Argumentos Insuficientes"
+		${BINDIR}/LoguearW5.sh "$3" "E" "Error al mover. Argumentos Insuficientes"
 		echo "Argumentos insuficientes" 1>&2
 		return 1
 	else
-		if [ -e $1 ]
+		if [ -e "$1" ]
 		then
-			if [ -d $2 ]
+			if [ -d "$2" ]
 			then
-				SEC=$(ls $2 | grep $(basename $1) | cut -d. -f3 | tail -1)
+				SEC=$(ls "$2" | grep "$(basename "$1")" | cut -d. -f3 | tail -1)
 				SEC=$(expr $SEC + 1)
 
-				NEWPATH=$2"/"$(basename $1)"."$SEC
-				mv $1 $NEWPATH
+				NEWPATH="$2""/"$(basename "$1")"."$SEC
+				mv "$1" "$NEWPATH"
 
 				# GUARDO EN EL ARCHIVO DE LOG
-				${BINDIR}/LoguearW5.sh $3 "I" "Copiado $1 a $2"
+				${BINDIR}/LoguearW5.sh "$3" "I" "Copiado $1 a $2"
 				return 0
 			else
 				
-				${BINDIR}/LoguearW5.sh $3 "E" "Error al mover. No existe el directorio $2"
+				${BINDIR}/LoguearW5.sh "$3" "E" "Error al mover. No existe el directorio $2"
 				echo "No existe el directorio $2" 1>&2
 				return 1
 			fi
 		else
-			${BINDIR}/LoguearW5.sh $3 "E" "Error al mover. No existe el archivo $1"
+			${BINDIR}/LoguearW5.sh "$3" "E" "Error al mover. No existe el archivo $1"
 			echo "No existe el archivo $1" 1>&2
 			return 1
 		fi
@@ -42,7 +42,7 @@ function moverW5 {
 ORIGEN=$1
 DESTINO=$2
 COMANDO=$3
-moverW5 $ORIGEN $DESTINO $COMANDO
+moverW5 "$ORIGEN" "$DESTINO" "$COMANDO"
 RET=$?
 
 exit $RET
