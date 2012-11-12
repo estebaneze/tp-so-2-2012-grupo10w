f9d5bd2bacc388b9f5ac3a63c2ff888deab3c436
lorenak@gmail.com <lorenak@gmail.com@3507be77-532f-1d92-cd08-bbd8338995c8>
2012-11-12 21:35:44 +0000

    la parte de busqueda por caracter

--- trunk/src/BuscarW5.sh
+++ trunk/src/BuscarW5.sh
@@ -79,122 +79,138 @@ while read linea; do
 		LoguearW5.sh "BuscarW5" "E" "Archivo ${linea} ya se encuentra procesado"
 		#comentar lo que sigue cuando se integre
 		MoverW5.sh "$ACEPDIR/$linea" "$RECHDIR" "BuscarW5"
-	fi
+	else
 
-	#Inicializo variables
-	conpatron=0
-	conhallazgo=0
+		#Inicializo variables
+		conpatron=0
+		conhallazgo=0
 
-	#Recorro archivo de patrones
-	while read lineap; do
+		#Recorro archivo de patrones
+		while read lineap; do
 		
-		#separo los campos
-		pat_id=$(echo $lineap | cut -d \, -f 1)
-		pat_exp=$(echo $lineap | cut -d \, -f 2)
-		pat_exp=$( echo "$pat_exp" | tr -d "\'")
-		sis_id=$(echo $lineap | cut -d \, -f 3)
-		pat_con=$(echo $lineap | cut -d \, -f 4)
-		desde=$(echo $lineap | cut -d \, -f 5)
-		hasta=$(echo $lineap | cut -d \, -f 6)
-		nhasta=$(echo $hasta | grep -o '[0-9][0-9]*')
-		sis_id1=$(echo $linea | cut -d \_ -f 1) 
+			#separo los campos
+			pat_id=$(echo $lineap | cut -d \, -f 1)
+			pat_exp=$(echo $lineap | cut -d \, -f 2)
+			pat_exp=$( echo "$pat_exp" | tr -d "\'")
+			sis_id=$(echo $lineap | cut -d \, -f 3)
+			pat_con=$(echo $lineap | cut -d \, -f 4)
+			desde=$(echo $lineap | cut -d \, -f 5)
+			hasta=$(echo $lineap | cut -d \, -f 6)
+			nhasta=$(echo $hasta | grep -o '[0-9][0-9]*')
+			sis_id1=$(echo $linea | cut -d \_ -f 1) 
 	
-		if [ $sis_id1 = $sis_id ];
-		then 	
-			conpatron=1
-			cant_hzgo_arch=0
-			separador="+-#-+"
-			nro_linea=0
+			if [ $sis_id1 = $sis_id ];
+			then 	
+				conpatron=1
+				cant_hzgo_arch=0
+				cant_hallados_caracter=0
+				separador="+-#-+"
+				nro_linea=0
 
-			#bus="grep -n "$pat_exp" "$ACEPDIR/$linea" >> .busqueda"
-			#eval $bus
-lalala="$ACEPDIR/$linea"
+				#bus="grep -n "$pat_exp" "$ACEPDIR/$linea" >> .busqueda"
+				#eval $bus
+	lalala="$ACEPDIR/$linea"
 
-			grep -n "${pat_exp}" "${lalala}" >> "${TEMPDIR}/.busqueda"
-#cat .busqueda
-			cant_hzgo_arch=$(wc -l < "${TEMPDIR}/.busqueda")
+				grep -n "${pat_exp}" "${lalala}" >> "${TEMPDIR}/.busqueda"
+	#cat .busqueda
+				cant_hzgo_arch=$(wc -l < "${TEMPDIR}/.busqueda")
 
-			if [ $cant_hzgo_arch -gt 0 ];
-			then
-				conhallazgo=1	
+				if [ $cant_hzgo_arch -gt 0 ];
+				then
+					conhallazgo=1	
 				
-				#calculo cuantos caracteres o lineas debo guardar	
-				cuantos=1
-				let cuantos+=nhasta
-				let cuantos-=desde
+					#calculo cuantos caracteres o lineas debo guardar	
+					cuantos=1
+					let cuantos+=nhasta
+					let cuantos-=desde
 				
-				#si tengo que guardar lineas
-				if [ "$pat_con" = "linea" ]; then			
-					while read linealog; do
-						nro_linealog=$(echo $linealog | cut -d \: -f 1)
-						let nro_linealog-=1
-						let nro_linealog+=desde
-						cuantosguardo=$cuantos
-						while [ $cuantosguardo -ne 0 ]; do
-							let cuantosguardo-=1
+					#si tengo que guardar lineas
+					if [ "$pat_con" = "linea" ]; then			
+						while read linealog; do
+							nro_linealog=$(echo $linealog | cut -d \: -f 1)
+							let nro_linealog-=1
+							let nro_linealog+=desde
+							cuantosguardo=$cuantos
+							while [ $cuantosguardo -ne 0 ]; do
+								let cuantosguardo-=1
 							
-							#hallar="head -$nro_linealog "$ACEPDIR/$linea" | tail -1" 
-							hallado=$(head -$nro_linealog "$ACEPDIR/$linea" | tail -1)
-							echo "${nro_ciclo}${separador}${linea}${separador}${nro_linealog}${separador}${hallado}" >> "${PROCDIR}/resultados.${pat_id}"			
-						let nro_linealog+=1						
-						done
-					done < "${TEMPDIR}/.busqueda"
+								#hallar="head -$nro_linealog "$ACEPDIR/$linea" | tail -1" 
+								hallado=$(head -$nro_linealog "$ACEPDIR/$linea" | tail -1)
+								echo "${nro_ciclo}${separador}${linea}${separador}${nro_linealog}${separador}${hallado}" >> "${PROCDIR}/resultados.${pat_id}"			
+							let nro_linealog+=1						
+							done
+						done < "${TEMPDIR}/.busqueda"
 				
-				#si tengo que guardar caracteres					
-				elif [ "$pat_con" = "caracter" ]; then
-				#cuento cantidad de archivos
-				cant_lineasarch=$(wc -l < "${TEMPDIR}/.busqueda")
-				for i in $(seq 1 $cant_lineasarch)
-					do
-						IFSOLD=$IFS
-						IFS="\n"
-						#obtengo nombre del primer archivo
-	    					linealog=$(head -n $i "${TEMPDIR}/.busqueda" | tail -n 1)
-						echo $linealog >> "${TEMPDIR}/.buslinea"
-						nro_linealog=$(grep -o "^[0-9][0-9]*" "${TEMPDIR}/.buslinea")
+					#si tengo que guardar caracteres					
+					elif [ "$pat_con" = "caracter" ]; then
+					#cuento cantidad de archivos
+					cant_lineasarch=$(wc -l < "${TEMPDIR}/.busqueda")
+					for i in $(seq 1 $cant_lineasarch)
+						do
+							IFSOLD=$IFS
+							IFS="\n"
+							#obtengo nombre del primer archivo
+		    					linealog=$(head -n $i "${TEMPDIR}/.busqueda" | tail -n 1)
+							echo $linealog >> "${TEMPDIR}/.buslinea"
+							nro_linealog=$(grep -o "^[0-9][0-9]*" "${TEMPDIR}/.buslinea")
 						
-						rm "${TEMPDIR}/.buslinea"
-						echo $linealog | sed "s/^[0-9][0-9]*://" >> "${TEMPDIR}/.bus" 2> "/dev/null"
+							rm "${TEMPDIR}/.buslinea"
+							echo $linealog | sed "s/^[0-9][0-9]*://" >> "${TEMPDIR}/.bus" 2> "/dev/null"
 
-						#expre="grep -bo $pat_exp .bus|cut -d\: -f 1" 
-						pos_hallado=$(grep -bo "$pat_exp" "${TEMPDIR}/.bus" | cut -d\: -f 1)
-						let pos_hallado+=1
-						hastacar=0
-						desdecar=0	
-						let desdecar+=pos_hallado
-						let desdecar+=desde
-						let desdecar-=1
-						let hastacar+=desdecar
-						let hastacar+=cuantos
-						let hastacar-=1
+							#expre="grep -bo $pat_exp .bus|cut -d\: -f 1" 
+							pos_hallado=$(grep -bo "$pat_exp" "${TEMPDIR}/.bus" | cut -d\: -f 1)
+							let pos_hallado+=1
+							hastacar=0
+							desdecar=0	
+							let desdecar+=pos_hallado
+							let desdecar+=desde
+							let desdecar-=1
+							let hastacar+=desdecar
+							let hastacar+=cuantos
+							let hastacar-=1
+							
+						
 
-						hallado=$(cat "${TEMPDIR}/.bus" | cut -c$desdecar-$hastacar) 
-						rm "${TEMPDIR}/.bus"
-						IFS=$IFSOLD
-						echo "${nro_ciclo}${separador}${linea}${separador}${nro_linealog}${separador}${hallado}" >> "${PROCDIR}/resultados.${pat_id}"			
-					done 
-				
-				fi  	
-			fi
-			rm "${TEMPDIR}/.busqueda"
-			echo "${nro_ciclo},${linea},${cant_hzgo_arch},${pat_exp},${pat_con},${desde},${hasta}" >> "${PROCDIR}/rglobales.${pat_id}"
-		fi	
+							hallado=$(cat "${TEMPDIR}/.bus" | cut -c$desdecar-$hastacar) 
+							
+							numcarac=$(echo "$hallado" | tr -d '\n' | wc -c)
+							#echo "num car: ${numcarac} cuantos ${cuantos} patron ${pat_id}"
 		
-	done < "$MAEDIR/patrones"	
+							rm "${TEMPDIR}/.bus"
 
-	if [ $conhallazgo -eq 1 ]; then 
-		let cant_hzgo+=1
-	else 
-		let cant_s_hzgo+=1
-	fi
+							IFS=$IFSOLD
+							if [ $numcarac -eq $cuantos ]; then
+							echo "${nro_ciclo}${separador}${linea}${separador}${nro_linealog}${separador}${hallado}" >> "${PROCDIR}/resultados.${pat_id}"					
+							let cant_hallados_caracter+=1
+							fi			
+						done 
+				
+					fi  	
+				fi
+				rm "${TEMPDIR}/.busqueda"
+				if [ $cant_hallados_caracter -ne 0 ]; then
+				echo "${nro_ciclo},${linea},${cant_hallados_caracter},${pat_exp},${pat_con},${desde},${hasta}" >> "${PROCDIR}/rglobales.${pat_id}"
+				else 
+					conhallazgo=0
+				fi
+			fi	
+		
+		done < "$MAEDIR/patrones"	
 
-	if [ $conpatron -eq 0 ]; then 
-		LoguearW5.sh "BuscarW5" "I" "No hay patrones aplicables para este archivo"
-		let cant_s_patron+=1
-	fi
+		if [ $conhallazgo -eq 1 ]; then 
+			let cant_hzgo+=1
+		else 
+			let cant_s_hzgo+=1
+		fi
 
-	#habilitar lo que sigue cuando se integre todo
-        MoverW5.sh "$ACEPDIR/$linea" "$PROCDIR" "BuscarW5"
+		if [ $conpatron -eq 0 ]; then 
+			LoguearW5.sh "BuscarW5" "I" "No hay patrones aplicables para este archivo"
+			let cant_s_patron+=1
+		fi
+
+		#habilitar lo que sigue cuando se integre todo
+		MoverW5.sh "$ACEPDIR/$linea" "$PROCDIR" "BuscarW5"
+		fi
 	
 done < "${TEMPDIR}/.temp_archivosB"
 
